f2684c8a45cae8907a305636f86eafb694634c09
Yug Bhatia <bhatiayug175@gmail.com>
2025-09-11 12:34:32 +0530

    Credentials secured

--- src/functions/githublogin.js
+++ src/functions/githublogin.js
@@ -1,5 +1,5 @@
 export default function loginWithGithub(){
-    const clientId = "Ov23liNIKGOX6tMb84AL";
+    const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
     const redirectUri = "https://readme-tales-frontend.vercel.app/auth/github/callback";
     const scope = "repo";
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`
